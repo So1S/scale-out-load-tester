29c78de4c482fa961364c823395ba612d57ca500
jiho lee <optional.int@kakao.com>
2022-05-30 05:09:03 +0900

    feat: set port to 8080

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    http.get('http://my-scale-out-test');
+    http.get('http://my-scale-out-test:8080');
     sleep(1);
 }
