619070b56fbe85f05a20e9dc40d41110588e220b
jiho lee <optional.int@kakao.com>
2022-05-31 17:18:25 +0900

    feat: get messages

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ export default function () {
     const body = JSON.stringify(obj);
 
     http.post('http://my-scale-out-test:8080/messages', body);
+    http.get('http://my-scale-out-test:8080/messages');
     sleep(0.01);
 }
